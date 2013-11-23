cac2dfdd70778d9f673db98897b72e92746d968f
paulo_vps4 <paulo@vps4>
2013-11-23 11:18:27 +0100

    improved docs

--- lib/bitstampede/BitstampPrivateApi.rb
+++ lib/bitstampede/BitstampPrivateApi.rb
@@ -16,30 +16,37 @@ module Bitstampede
     end
     alias_method :transactions, :user_transactions
 
+    # @return [Array<Entities::UserOrder>]
     def user_orders
       mapper.map_user_orders(net.make_request_and_expect_json(:POST,"open_orders"))
     end
     alias_method :orders, :user_orders
 
-    def buy!(amount, price)
-      trade!("buy", amount, price)
+    # Open new buy-limit order to buy *amount_btc* for *price_usd*
+    # @return {Entities::UserOrder}
+    def buy!(amount_btc, price_usd)
+      trade!("buy", amount_btc, price_usd)
     end
 
-    def sell!(amount, price)
-      trade!("sell", amount, price)
+    # Open new sell-limit order to sell *amount_btc* for *price_usd*
+    # @return {Entities::UserOrder}
+    def sell!(amount_btc, price_usd)
+      trade!("sell", amount_btc, price_usd)
     end
 
-    def cancel!(id)
+    # Cancel an existing order (buy-limit or sell-limit), with *order_id*
+    # @return [TrueClass, FalseClass] true or false
+    def cancel!(order_id)
       wrapping_standard_error do
-        mapper.map_cancel(net.make_request_and_expect_json(:POST,"cancel_order", { id: id.to_s }))
+        mapper.map_cancel(net.make_request_and_expect_json(:POST,"cancel_order", { order_id: order_id.to_s }))
       end
     end
 
     private
 
-    def trade!(type, amount, price)
+    def trade!(type, amount_btc, price_usd)
       wrapping_standard_error do
-        mapper.map_user_order(net.make_request_and_expect_json(:POST,type, { price: price.to_digits, amount: amount.to_digits }))
+        mapper.map_user_order(net.make_request_and_expect_json(:POST,type, { price_usd: price_usd.to_digits, amount_btc: amount_btc.to_digits }))
       end
     end
 
